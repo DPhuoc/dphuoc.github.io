41031cd340ca35a44428bf462e65b8b1e37d7a24
DPhuoc <phuoc7c@gmail.com>
2024-07-01 11:47:06 +0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://dphuoc.github.io",
   base: "/",
   trailingSlash: "always",
   integrations: [
